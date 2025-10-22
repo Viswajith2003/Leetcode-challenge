994493ce976e5e451ac427a60a6fe244b0bd0ceb
Viswajith2003 <viswajithviswa715@gmail.com>
2025-10-22 06:34:20 +0530

    reverse a string

--- revStr.js
+++ revStr.js
@@ -1,23 +1,32 @@
+
+// reverse a string
+
+str = "viswajith";
+let rev = [...str].reverse().join("");
+console.log(rev);
+
+
+
 // Example :
 // Input: s = ["h","e","l","l","o"]
 // Output: ["o","l","l","e","h"]
-// Example 2:
 
-// Input: s = ["H","a","n","n","a","h"]
-// Output: ["h","a","n","n","a","H"]
 
-function revStr(s) {
-  let i = 0;
-  let j = s.length - 1;
+// function revStr(s) {
+//   let i = 0;
+//   let j = s.length - 1;
 
-  while (i < j) {
-    let temp = s[i];
-    s[i] = s[j];
-    s[j] = temp;
-    i++;
-    j--;
-  }
-  return s;
-}
-s = ["h", "e", "l", "l", "o"];
-console.log(revStr(s));
+//   while (i < j) {
+//     let t = s[i];
+//     s[i] = s[j];
+//     s[j] = t;
+//     i++;
+//     j--;
+//   }
+//   return s;
+// }
+
+// s = ["h", "e", "l", "l", "o"];
+// console.log(revStr(s));
+
+
