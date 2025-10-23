be980c0c331ed44dfde58c96aa8b4883c8e8ad76
Viswajith2003 <viswajithviswa715@gmail.com>
2025-10-23 10:14:55 +0530

    is prime

---
+++ prime.js
@@ -0,0 +1,14 @@
+function isPrime(num) {
+  if (num <= -1) {
+    return false;
+  } else {
+    for (let i = 2; i <= num / 2; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(isPrime(9));
